033504e203bc454f776d8977daf1651f2c53a404
Ce Zheng <ce.zheng@gree.net>
2015-01-23 22:10:02 +0900

    oo20150123

---
+++ Tu/Frontend/XMPPUserCoreDataStorageObject+Riot.h
@@ -0,0 +1,13 @@
+//
+//  XMPPUserCoreDataStorageObject+Riot.h
+//  Tu
+//
+//  Created by Ce Zheng on 1/23/15.
+//  Copyright (c) 2015 Ce Zheng. All rights reserved.
+//
+
+#import "XMPPUserCoreDataStorageObject.h"
+
+@interface XMPPUserCoreDataStorageObject (Riot)
+
+@end
---
+++ Tu/Frontend/XMPPUserCoreDataStorageObject+Riot.m
@@ -0,0 +1,13 @@
+//
+//  XMPPUserCoreDataStorageObject+Riot.m
+//  Tu
+//
+//  Created by Ce Zheng on 1/23/15.
+//  Copyright (c) 2015 Ce Zheng. All rights reserved.
+//
+
+#import "XMPPUserCoreDataStorageObject+Riot.h"
+
+@implementation XMPPUserCoreDataStorageObject (Riot)
+
+@end
