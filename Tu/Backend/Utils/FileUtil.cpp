cac9ecd43cba65a5cd7828cc99217d5d73b15c63
Ce Zheng <cezheng.cs@gmail.com>
2015-01-31 23:58:35 +0900

    cleanup unused files

--- Tu/Backend/Utils/FileUtil.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  FileUtil.cpp
-//  Tu
-//
-//  Created by Ce Zheng on 8/12/14.
-//  Copyright (c) 2014 Ce Zheng. All rights reserved.
-//
-
-#include "FileUtil.h"
--- Tu/Service/XMPPClient.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  XMMPClient.cpp
-//  Tu
-//
-//  Created by Ce Zheng on 11/27/14.
-//  Copyright (c) 2014 Ce Zheng. All rights reserved.
-//
-
-#include "XMMPClient.h"
