65deb46868c7f5836b173ce6fec894f9990a103e
cezheng <cezheng.cs@gmail.com>
2015-02-05 20:19:30 +0900

    add fetch champion image list

--- Tu/XPFService/Services/RiotAPI/RiotAssetManager.cpp
+++ Tu/XPFService/Services/RiotAPI/RiotAssetManager.cpp
@@ -1,6 +1,8 @@
 #include "RiotAssetManager.h"
+#include "RiotAPI.h"
 #include "XPFService/Utils/Downloader.h"
 #include "XPFService/Utils/Exception.h"
+#include "XPFService/Utils/KVS.h"
 
 using XPF::CurlRequest;
 using XPF::CurlResponse;
@@ -11,6 +13,7 @@ const char * RiotAssetManager::baseUrl = "http://ddragon.leagueoflegends.com";
 const URLPattern RiotAssetManager::versionUrl = {"/realms/{region}.json", NULL, {}, {}};
 const char * RiotAssetManager::profileImageUrlPattern = "/cdn/{version}/img/profileicon/{icon_id}.png";
 
+//general
 
 void RiotAssetManager::updateVersionInfo(Region region) {
     CurlRequest request;
@@ -20,23 +23,43 @@ void RiotAssetManager::updateVersionInfo(Region region) {
     _versionInfo[region] = Json::parse(res.data, err);
 }
 
+//profileImage
+
 std::string RiotAssetManager::getProfileIconVersion(Region region) const {
     XPF::REQUIRE(_versionInfo.find(region) != _versionInfo.end(), "riot version info not updated yet");
     return _versionInfo.at(region)["n"]["profileicon"].string_value();
 }
 
-std::string RiotAssetManager::getProfileIconUrl(long id, Region region) {
+std::string RiotAssetManager::getProfileIconUrl(long id, Region region) const {
     URLPattern urlPattern(profileImageUrlPattern, getProfileIconVersion(region).c_str(), {}, {});
     return baseUrl + urlPattern.generateURL(region, {{"icon_id", std::to_string(id)}});
 }
 
-std::string RiotAssetManager::getProfileIconPath(long id, Region region) {
+std::string RiotAssetManager::getProfileIconPath(long id, Region region) const {
     auto downloader = XPF::Downloader::getInstance();
     std::string url = getProfileIconUrl(id, region);
     if (downloader->isDownloaded(url)) {
         return downloader->makeDownloadPath(url);
     }
     return downloader->downloadUrl(url, std::string(regionStrings[region]) + "#profileIcon" + std::to_string(id));
+}
+
+//champion image
+
+void RiotAssetManager::updateChampionImageInfoList(Region region) {
+    XPF::CurlRequest request;
+    RiotAPI* api = RiotAPIHolder::getInstance()->getAPIByRegion(region);
+    Json res = api->getChampionList({
+        {"champData", "image"},
+        {"dataById" , "true"}
+    });
+    printf("dump champion image list %s\n", res.dump().c_str());
+    _championImageInfoList[region] = res;
+}
+
+Json RiotAssetManager::getChampionImageInfo(long id, Region region) const {
+    XPF::REQUIRE(_championImageInfoList.find(region) != _championImageInfoList.end(), "riot champion image list not fetched yet");
+    return _championImageInfoList.at(region)["data"][std::to_string(id)];
 }
 
 NS_RIOT_END
--- Tu/XPFService/Services/RiotAPI/RiotAssetManager.h
+++ Tu/XPFService/Services/RiotAPI/RiotAssetManager.h
@@ -9,18 +9,21 @@ NS_RIOT_BEGIN
 
 class RiotAssetManager {
 public:
-    typedef json11::Json VersionInfo;
     static const char * baseUrl;
     static const URLPattern versionUrl;
     static const char* profileImageUrlPattern;
     
     void updateVersionInfo(Region region = NA);
     std::string getProfileIconVersion(Region region = NA) const;
-    std::string getProfileIconUrl(long id, Region region = NA);
-    std::string getProfileIconPath(long id, Region region = NA);
+    std::string getProfileIconUrl(long id, Region region = NA) const;
+    std::string getProfileIconPath(long id, Region region = NA) const;
+    
+    void updateChampionImageInfoList(Region region = NA);
+    json11::Json getChampionImageInfo(long id, Region region = NA) const;
 protected:
 private:
-    std::unordered_map<Region, VersionInfo, std::hash<unsigned char>> _versionInfo;
+    std::unordered_map<Region, json11::Json, std::hash<unsigned char>> _versionInfo;
+    std::unordered_map<Region, json11::Json, std::hash<unsigned char>> _championImageInfoList;
 };
 
 NS_RIOT_END
