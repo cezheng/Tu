6f1cfddfba162fd046e4761473333995a2a336b7
cezheng <cezheng.cs@gmail.com>
2015-03-06 18:10:29 +0900

    add reader writer lock to leveldb holder

--- Tu/XPFService/Base/LevelDBHolder.cpp
+++ Tu/XPFService/Base/LevelDBHolder.cpp
@@ -1,33 +1,108 @@
 #include "LevelDBHolder.h"
 #include "XPFService/Utils/FileUtil.h"
 #include "XPFService/Utils/Exception.h"
+#include "leveldb/write_batch.h"
+
+using json11::Json;
 
 NS_XPF_BEGIN
 
 LevelDBHolder::LevelDBHolder(leveldb::DB * db) : _db(db) {
-    
+    pthread_rwlock_init(&_rwlock, NULL);
 }
 
 LevelDBHolder::~LevelDBHolder() {
+    pthread_rwlock_destroy(&_rwlock);
     delete _db;
 }
 
 bool LevelDBHolder::set(const std::string & key, const std::string & value) {
+    pthread_rwlock_wrlock(&_rwlock);
     leveldb::Status s = _db->Put(leveldb::WriteOptions(), key, value);
+    pthread_rwlock_unlock(&_rwlock);
     return s.ok();
 }
 
 bool LevelDBHolder::get(const std::string & key, std::string & value) {
+    pthread_rwlock_rdlock(&_rwlock);
     leveldb::Status s = _db->Get(leveldb::ReadOptions(), key, &value);
+    pthread_rwlock_unlock(&_rwlock);
+    return s.ok();
+}
+
+bool LevelDBHolder::getJson(const std::string & key, Json & json) {
+    pthread_rwlock_rdlock(&_rwlock);
+    std::string value;
+    leveldb::Status s = _db->Get(leveldb::ReadOptions(), key, &value);
+    pthread_rwlock_unlock(&_rwlock);
+    if(!s.ok()) {
+        return false;
+    }
+    std::string err;
+    json = Json::parse(value, err);
+    return err.empty();
+}
+
+bool LevelDBHolder::getMutiple(json11::Json::object & object, std::size_t begin, std::size_t n) {
+    pthread_rwlock_rdlock(&_rwlock);
+    leveldb::Iterator* it = _db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid() && n; it->Next()) {
+        if (begin > 0) {
+            --begin;
+            continue;
+        }
+        std::string err;
+        auto item = Json::parse(it->value().ToString(), err);
+        if (err.empty()) {
+            object[it->key().ToString()] = item;
+            --n;
+        }
+    }
+    delete it;
+    pthread_rwlock_unlock(&_rwlock);
+    return true;
+}
+
+bool LevelDBHolder::setMutiple(const json11::Json::object &object) {
+    leveldb::WriteBatch writeBatch;
+    for (auto & kv : object) {
+        writeBatch.Put(kv.first, kv.second.dump());
+    }
+    pthread_rwlock_wrlock(&_rwlock);
+    leveldb::Status s = _db->Write(leveldb::WriteOptions(), &writeBatch);
+    pthread_rwlock_unlock(&_rwlock);
+    return s.ok();
+}
+
+bool LevelDBHolder::clear() {
+    pthread_rwlock_wrlock(&_rwlock);
+    leveldb::WriteBatch writeBatch;
+    leveldb::Iterator* it = _db->NewIterator(leveldb::ReadOptions());
+    it->SeekToFirst();
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        writeBatch.Delete(it->key());
+    }
+    delete it;
+    leveldb::Status s = _db->Write(leveldb::WriteOptions(), &writeBatch);
+    pthread_rwlock_unlock(&_rwlock);
+    return s.ok();
+}
+
+bool LevelDBHolder::remove(const std::string &key) {
+    pthread_rwlock_wrlock(&_rwlock);
+    leveldb::Status s = _db->Delete(leveldb::WriteOptions(), key);
+    pthread_rwlock_unlock(&_rwlock);
     return s.ok();
 }
 
 void LevelDBHolder::init() {
     leveldb::Options options;
     options.create_if_missing = true;
+    pthread_rwlock_wrlock(&_rwlock);
     FileUtil::getInstance()->createDirectory(getLevelDBBasePath(), 0755);
     leveldb::Status status = leveldb::DB::Open(options, getLevelDBFullPath(), &_db);
     REQUIRE(status.ok(), status.ToString());
+    pthread_rwlock_unlock(&_rwlock);
 }
 
 std::string LevelDBHolder::getLevelDBFullPath() const {
--- Tu/XPFService/Base/LevelDBHolder.h
+++ Tu/XPFService/Base/LevelDBHolder.h
@@ -2,7 +2,9 @@
 #define __Tu__LevelDBHolder__
 
 #include "XPFService/XPFDefine.h"
+#include "json11/json11.hpp"
 #include "leveldb/db.h"
+#include "pthread.h"
 
 NS_XPF_BEGIN
 
@@ -12,12 +14,18 @@ public:
     virtual ~LevelDBHolder();
     bool set(const std::string & key, const std::string & value);
     bool get(const std::string & key, std::string & value);
+    bool getJson(const std::string & key, json11::Json & json);
+    bool getMutiple(json11::Json::object & object, std::size_t begin = 0, std::size_t n = INT64_MAX);
+    bool setMutiple(const json11::Json::object & object);
+    bool clear();
+    bool remove(const std::string & key);
 protected:
     virtual void init();
     leveldb::DB* _db;
     virtual std::string getLevelDBBasePath() const = 0;
     virtual std::string getLevelDBFileName() const = 0;
     virtual std::string getLevelDBFullPath() const;
+    pthread_rwlock_t _rwlock;
 };
 
 NS_XPF_END
